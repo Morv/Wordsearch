6acd650dff4d1dbb474c9b665345a7af7b177ec1
Morvan G. Perez <morvan02@yahoo.com>
2013-09-24 18:03:12 -0400

    source.c

---
+++ source.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define flush fflush(stdin)
+#define pause system("pause")
+
+
+//PROTOTYPES
+int fits(*randX, *randY, *randD);
+//MAIN
+
+
+main() {
+//create gameboard	[25][50]
+//create word array [14][9]
+
+	randX=0;
+	randY=0;
+	randD=0;
+
+
+	fitsFunction(*randX, *randY, *randD)
+		
+	//getGameWords
+	while placement = 0
+	{
+		//FITS  random location <------< <-------------------------------------------<<
+		//FITS  random direction  <----^ <---<  //flag direction.. if all are flagged ^
+		//FITS  check if it fits---^     ^
+			
+		//placeTheWord(randD, array, randX, randY);
+	}
+	//all words placed? no-->word[i+1]
+	
+	//fill empty slots with random letters.
+
+	//display game board
+	//display words in 2 columns.
+	//printf("1. blah blah       8. blah blah");
+	//printf("2. blah blah		 9. blah blah");
+ pause;
+}
+//get game Words
+	//make sure length <= 8 and A-Z or a-z
+
+int fits(*randX, *randY, *randD) {
+	int i =0;
+
+	if fits
+	(i=1);
+
+
+	return i;
+}
